013ee436c98e9bca4216f5d8c7e06fc260552d75
gp900 <137711156+gp900@users.noreply.github.com>
2023-06-27 23:41:03 +0530

    Add files via upload

---
+++ Assigment2Operator7.py
@@ -0,0 +1,15 @@
+# using membership operator
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+
+# check if '1' key is present in y
+print(1 in y)  # prints True
+
+# check if 'a' key is present in y
+print('a' in y)  # prints False
